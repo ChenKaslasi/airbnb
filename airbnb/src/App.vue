81b6614081badc44b5e8183e31a8d402ee9423ad
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-11-26 00:38:14 +0200

    details-page init

--- airbnb/src/App.vue
+++ airbnb/src/App.vue
@@ -21,10 +21,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  text-align: center;
-}
-
-</style>
--- airbnb/src/views/space-details.vue
+++ airbnb/src/views/space-details.vue
@@ -1,12 +1,36 @@
 <template>
-  <section>
-      <h1>space details</h1>
+  <section class="deails-container ">
+    <gallery :space = currSpace />
+    <!-- <pre>{{currSpace}}</pre> -->
+      <section >
+        <section class="main-content"></section>
+        <section class="checkout card"></section>
+      </section>
+
+      <section class="reviews"></section>
+      <section class="host"></section>
+      <section class="more"></section>
   </section>
 </template>
 
 <script>
-export default {
+import spaceService from '../services/space.service.js'
+import gallery from "../cmps/space-gallery.cmp"
 
+export default {
+  components: {
+    gallery
+},
+data() {
+    return {
+      currSpace: null,
+    };
+  },
+  created() {
+    // const spaceId = this.$route.params.id;
+    const spaceId = 1001265;
+    spaceService.getById(spaceId).then((space) => (this.currSpace = space));
+  },
 }
 </script>
 
