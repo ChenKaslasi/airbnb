df7c87658a56d14510a99b63b03ff033e509842a
Roy Margalit <roy4499@gmail.com>
2020-11-26 00:46:08 +0200

    merge end of day 1

--- airbnb/src/cmps/space-filter.cmp.vue
+++ airbnb/src/cmps/space-filter.cmp.vue
@@ -1,28 +1,88 @@
 <template>
   <section>
-      <h1>filterrrr</h1>
-      <input v-model="filterBy" type="text" />
-    <button @click="emitFilter">push</button>
+    <div class="cover">
+      <form class="flex-form">
+        <input
+          type="search"
+          placeholder="Where do you wanna go?"
+          v-model="filterBy"
+        />
+        <label class="label-search" for="from">From</label>
+        <input type="date" name="from" />
+        <label class="label-search" for="to">To</label>
+        <input type="date" name="to" />
+        <select name="guests" id="guests">
+          <option value="1">1 guest</option>
+          <option value="2">2 guests</option>
+          <option value="3">3 guests</option>
+          <option value="4">4 guests</option>
+          <option value="5">5 guests</option>
+        </select>
+
+        <input
+          type="submit"
+          class="search-btn"
+          value="Search"
+          @click="emitFilter"
+        />
+      </form>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      filterBy:null
-    }
+  data() {
+    return {
+      filterBy: null,
+    };
   },
-  methods:{
-    emitFilter(){
-      this.$emit('filter',this.filterBy)
-      console.log('filter', this.filterBy);
+  methods: {
+    emitFilter() {
+      this.$emit("filter", this.filterBy);
+      console.log("filter", this.filterBy);
     },
-  }
-
-}
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+.cover {
+  // background: #cac1bc;
+  // position: sticky;
+  width: 100vw;
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.flex-form {
+  display: flex;
+  // width: 100%;
+
+}
+
+input[type="search"] {
+  flex-basis: 350px;
+  border-radius: 30px 0px 0px 30px;
+}
+
+.flex-form > * {
+  border: 0;
+  padding: 10px;
+  line-height: 50px;
+  font-size: 20px;
+  border-radius: 0;
+  outline: 0;
+  background: #fff;
+  margin-left: 1px;
+}
+
+.flex-form input[type="submit"] {
+  background: #f05;
+  color: white;
+  border-radius:  0px 30px 30px 0px ;
+
+}
 </style>
--- airbnb/src/views/home-page.vue
+++ airbnb/src/views/home-page.vue
@@ -1,30 +1,108 @@
 <template>
   <div class="home">
-    <space-filter  @filter="setFilter" />
+    <div class="hero">
+      <space-filter @filter="setFilter" />
+      <div class="home-container">
+        <div class="location-container">
+          <div
+            class="card"
+            v-for="(img, idx) in imgs"
+            :key="idx"
+            @click="bookLocation(img.title)"
+          >
+            <template>
+              <img :src="img.url" />
+              <h3>{{ img.title }}</h3>
+              <p>Book it NOW!</p>
+            </template>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import spaceFilter from '../cmps/space-filter.cmp.vue'
+import spaceFilter from "../cmps/space-filter.cmp.vue";
 // @ is an alias to /src
 
 export default {
   name: "Home",
   components: {
-    spaceFilter
+    spaceFilter,
   },
   data() {
     return {
-    }
+      imgs: [
+        {
+          title: "Barcelona",
+          url: "https://wallpaperaccess.com/full/1471128.jpg",
+        },
+        {
+          title: "New York",
+          url:
+            "https://www.nyhabitat.com/blog/wp-content/uploads/2014/12/Top-5-sunset-spots-nyc-empire-state-skyline.jpg",
+        },
+        {
+          title: "Sydney",
+          url:
+            "https://hdwallpaperim.com/wp-content/uploads/2017/09/16/51093-cityscape-Sydney-Sydney_Opera_House-night-Australia.jpg",
+        },
+      ],
+    };
   },
   methods: {
     setFilter(filterBy) {
-      // this.$store.dispatch({
-      //   type:'filterSpaces',
-      //   filterBy
-      // })
-      this.$router.push(`/${filterBy}`)
+      this.$router.push(`/${filterBy}`);
+    },
+    bookLocation(title) {
+      this.$router.push(`/${title}`);
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+.hero {
+  height: 100vh;
+  width: 100vw;
+  // background-color: lightpink;
+  background-image: url("https://img5.goodfon.com/wallpaper/nbig/c/a5/nature-house-lake-mountains-village-saksun-faroe-islands-arc.jpg  ");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.card {
+  // border: 2px solid black;
+  text-align: start;
+  & h3 {
+    padding: 10px;
+    padding-left: 10px;
+    font-weight: 800;
+    font-size: 1.3rem;
+  }
+}
+.home-container {
+  margin-top: 10px;
+  .location-container {
+    display: flex;
+    flex-wrap: wrap;
+    & .card {
+      margin: 0 auto;
+      flex-direction: row;
+      & img {
+        width: 307px;
+        height: 204px;
+        border-radius: 15px;
+      }
+      & p {
+        color: rgb(136, 134, 134);
+        padding-left: 10px;
+      }
+      & :hover {
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>
