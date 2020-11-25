95064c8533e61a53c668aa9d9ded36cb3df4b129
Elad Rahamim <eladr14@gmail.com>
2020-11-25 16:48:28 +0200

    font

--- airbnb/src/main.js
+++ airbnb/src/main.js
@@ -6,9 +6,10 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import './styles/styles.scss'
+
 new Vue({
   router,
   store,
-  render: h => h(app) 
+  render: h => h(app)
 }).$mount('#app')
-  
