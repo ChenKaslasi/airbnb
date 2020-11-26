f5d5f19792a710f24e38b0edcb103b6916c9a485
Roy Margalit <roy4499@gmail.com>
2020-11-26 16:55:09 +0200

    reviews korkinet

--- airbnb/src/App.vue
+++ airbnb/src/App.vue
@@ -5,18 +5,18 @@
       <!-- <router-link to="/">Home</router-link> | -->
       <router-view />
     </main>
-    <space-footer />
+    <!-- <space-footer /> -->
   </div> 
 </template>
 
 <script>
 // import spaceHeader from "@/cmps/space-header.cmp.vue";
-import spaceFooter from "@/cmps/space-footer.cmp.vue";
+// import spaceFooter from "@/cmps/space-footer.cmp.vue";
 
 export default {
   components: {
     // spaceHeader,
-    spaceFooter,
+    // spaceFooter,
   },
 };
 </script>
