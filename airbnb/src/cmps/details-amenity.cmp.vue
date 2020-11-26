dd8bed39c3000206b61e6cec3eb21f084a2dd44a
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-11-27 01:32:29 +0200

    amenities update

--- airbnb/src/cmps/details-amenity.cmp.vue
+++ airbnb/src/cmps/details-amenity.cmp.vue
@@ -2,67 +2,9 @@
   <section class="amenity-container">
     <h1 class="title">Amenities</h1>
     <div class="box">
-      <ul
-        class="amenity-list"
-        v-for="(amenity, idx) in space.amenities"
-        :key="idx">
-        <li class="amenity" :class="{hidden: amenity !== 'TV' }">
-          <img src="@/assets/icons/tv.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-        <li class="amenity" v-if="amenity === 'Wifi'">
-          <img src="@/assets/icons/wifi.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-        <li class="amenity" v-if="amenity === 'Air conditioning'">
-          <img src="@/assets/icons/air.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-        <li class="amenity" v-if="amenity === 'Pool'">
-          <img src="@/assets/icons/pool.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-
-        <li class="amenity" v-if="amenity === 'Free parking on premises'">
-          <img src="@/assets/icons/parking.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-
-        <li class="amenity" v-if="amenity === 'Kitchen'">
-          <img src="@/assets/icons/kitchen.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-
-        <li class="amenity" v-if="amenity === 'Elevator'">
-          <img src="@/assets/icons/elevator.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-
-        <li class="amenity" v-if="amenity === 'Dryer'">
-          <img src="@/assets/icons/dryer.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-
-        <li class="amenity" v-if="amenity === 'Iron'">
-          <img src="@/assets/icons/iron.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-
-        <li class="amenity" v-if="amenity === 'Essentials'">
-          <img src="@/assets/icons/essentials.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-
-        <li class="amenity" v-if="amenity === 'Hangers'">
-          <img src="@/assets/icons/hangers.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-        <li class="amenity" v-if="amenity === 'Hot tub'">
-          <img src="@/assets/icons/tub.svg" />
-          <span>{{ amenity }}</span>
-        </li>
-        <li class="amenity" v-if="amenity === 'Laptop friendly workspace'">
-          <img src="@/assets/icons/workspace.svg" />
+      <ul class="amenity-list" v-for="(amenity, idx) in space.amenities" :key="idx">
+        <li v-if="idx<12" class="amenity">
+          <img :src="require(`../assets/icons/${amenity}.svg`)" />
           <span>{{ amenity }}</span>
         </li>
       </ul>
@@ -75,8 +17,11 @@ export default {
   props: {
     space: Object,
   },
+  computed: {
+    
+  }
 };
 </script>
-
+  
 <style>
 </style>
