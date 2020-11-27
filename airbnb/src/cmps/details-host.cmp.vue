47f68860f01feb86b81af42e8571e9fb501f0954
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-11-27 12:39:03 +0200

    host update

--- airbnb/src/cmps/details-host.cmp.vue
+++ airbnb/src/cmps/details-host.cmp.vue
@@ -1,30 +1,34 @@
 <template>
   <section class="host-container">
     <section class="top">
-    <div class="thumbnail">
-      <img :src="space.host.profileImg" />
-    </div>
-     <span class="title">{{ `Hosted by ${space.host.name}`}}</span>
-     <!-- Todo : add host joined date to db -->
+      <div class="thumbnail">
+        <img :src="space.host.profileImg" />
+      </div>
+      <span class="title">{{ `Hosted by ${space.host.name}` }}</span>
+      <!-- Todo : add host joined date to db -->
       <!-- <span class="info">{{`Joined in ${space}`}}</span> -->
-      <span class="info">{{`Joined in March 2013`}}</span>
-
+      <span class="info">{{ `Joined in March 2013` }}</span>
     </section>
-      
-    <section>
-
+    <section class="content">
+      <div class="reviews">
+        <span class="star">󰀄</span>
+        <span > {{ `${space.reviews.length} Reviews` }} </span>
+      </div>
+      <div class="identity flex">
+        <img src="../assets/icons/verified.svg">
+        <span>Identity verified </span>
+      </div>
     </section>
   </section>
 </template>
 
 <script>
 export default {
- props: {
+  props: {
     space: Object,
   },
-}
+};
 </script>
 
 <style>
-
 </style>
