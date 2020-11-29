3e1f4f445577e879e0c85e3cb11dff1dbc122464
Elad Rahamim <eladr14@gmail.com>
2020-11-29 23:13:39 +0200

    day 5 1st frontend-backend commit

--- frontend/src/services/space.service.js
+++ frontend/src/services/space.service.js
@@ -1,31 +1,30 @@
 import httpService from './http.service.js'
 
 export default {
-  getSpaces,
+  add,
+  remove,
   getById,
-  filterSpaces,
   query
-}
-function getSpaces() {
-  return httpService.get('spaces')
 }
 function getById(spaceId) {
   return httpService.get(`spaces/${spaceId}`)
 }
 
-function filterSpaces(filterBy={}){
-  return httpService.get('spaces',filterBy)
+function remove(spaceId) {
+  return httpService.delete(`space/${spaceId}`)
 }
-
+function add(space) {
+  return httpService.post(`space`, space)
+}
 
 function query(filter = null) {
   var filterStr = '';
   if (filter) {
-      filter.toLowerCase()
-      // const searchStr = (filter.txt) ? `&q=${filter.txt}` : '';
-      // const typeStr = (filter.type === 'All') ? '' : `&type=${filter.type}`;
-      // const stockStr = (filter.stock) ? `&inStock=true` : '';
-      filterStr = filter
+    filter.toLowerCase()
+    // const searchStr = (filter.txt) ? `&q=${filter.txt}` : '';
+    // const typeStr = (filter.type === 'All') ? '' : `&type=${filter.type}`;
+    // const stockStr = (filter.stock) ? `&inStock=true` : '';
+    filterStr = filter
   }
   return httpService.get(`spaces?address.city=${filterStr}`)
 }
