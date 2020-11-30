468173afd1e4354534741e9740840886f833c7cb
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-11-30 15:12:12 +0200

    front back update

--- frontend/src/services/space.service.js
+++ frontend/src/services/space.service.js
@@ -7,25 +7,17 @@ export default {
   query
 }
 function getById(spaceId) {
-  return httpService.get(`spaces/${spaceId}`)
+  return httpService.get(`api/space/${spaceId}`)
 }
 
 function remove(spaceId) {
-  return httpService.delete(`space/${spaceId}`)
+  return httpService.delete(`api/space/${spaceId}`)
 }
 function add(space) {
-  return httpService.post(`space`, space)
+  return httpService.post(`api/space`, space)
 }
 
-function query(filter = null) {
-  var filterStr = '';
-  if (filter) {
-    filter.toLowerCase()
-    // const searchStr = (filter.txt) ? `&q=${filter.txt}` : '';
-    // const typeStr = (filter.type === 'All') ? '' : `&type=${filter.type}`;
-    // const stockStr = (filter.stock) ? `&inStock=true` : '';
-    filterStr = filter
-  }
-  return httpService.get(`spaces?address.city=${filterStr}`)
+function query() {
+    return httpService.get('api/space')
 }
 
