d0b75ba4b5028e511b5e8c0521920c963d57931a
Chen Kaslasi <ChenKaslasi@gmail.com>
2020-12-06 23:04:44 +0200

    loading screen update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,18 +4,18 @@
     <main>
       <router-view :key="$route.name + ($route.params.id || '')" />
     </main>
-    <space-footer />
+    <!-- <space-footer /> -->
   </div> 
 </template>
 
 <script>
 // import spaceHeader from "@/cmps/space-header.cmp.vue";
-import spaceFooter from "@/cmps/space-footer.cmp.vue";
+// import spaceFooter from "@/cmps/space-footer.cmp.vue";
 
 export default {
   components: {
     // spaceHeader,
-    spaceFooter,
+    // spaceFooter,
   },
 };
 </script>
