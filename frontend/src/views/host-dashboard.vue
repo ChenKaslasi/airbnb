22528bb2b8c0c1358f2aeb55dc2edf9b9868fe78
Roy Margalit <roy4499@gmail.com>
2020-12-06 19:53:55 +0200

    sunday 4

--- frontend/src/views/host-dashboard.vue
+++ frontend/src/views/host-dashboard.vue
@@ -7,8 +7,8 @@
           <div class="top">Pending / Accepted</div>
           <div class="bottom">
             <span
-              >{{ orders.length + 3 }} new items · {{ getResponseRate }} response
-              rate</span
+              >{{ orders.length + 3 }} new items ·
+              {{ getResponseRate }} response rate</span
             >
           </div>
         </div>
@@ -113,8 +113,10 @@
               <div class="value">40</div>
             </div>
           </div>
+          <host-svg />
         </div>
       </div>
+
       <!-- {{this.orders}} -->
     </section>
   </section>
@@ -123,10 +125,12 @@
 <script>
 import spaceService from "../services/space.service.js";
 import spaceHeader from "../cmps/space-header.cmp.vue";
+import hostSvg from "../cmps/host-svg.cmp.vue";
 import moment from "moment";
 export default {
   components: {
     spaceHeader,
+    hostSvg,
   },
   data() {
     return {
@@ -136,8 +140,8 @@ export default {
   },
   methods: {
     getDates(dates) {
-      return `${moment((dates.checkIn)).format("ll")} - ${moment(
-        (dates.checkOut)
+      return `${moment(dates.checkIn).format("ll")} - ${moment(
+        dates.checkOut
       ).format("ll")}`;
     },
     getSpaceName(spaceId) {
@@ -159,11 +163,11 @@ export default {
     },
   },
   async created() {
-    if(!sessionStorage.user) {
-      this.$router.push('/')
-      return
+    if (!sessionStorage.user) {
+      this.$router.push("/");
+      return;
     }
-    const activeUser = JSON.parse(sessionStorage.user) ;
+    const activeUser = JSON.parse(sessionStorage.user);
     this.username = activeUser.username; //get user name
     const userId = { id: activeUser._id };
     await this.$store.dispatch({
